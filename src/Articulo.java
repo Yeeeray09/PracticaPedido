96ee978ff59ddcfe950c2c817c12fc789e8fb0b9
Yeray Musila <yeraymf05@gmail.com>
2026-02-05 15:17:55 +0000

    añadido los atributos , constructor , un metodo para mostrar el articulo y getters para devolver valor

--- src/Articulo.java
+++ src/Articulo.java
@@ -1,3 +1,28 @@
 public class Articulo {
+    private String nombreArt;
+    private int cantidad;
+    private double PrecioU;
+
+    public Articulo(String nombreArt , int cantidad , double PrecioU){
+        this.nombreArt=nombreArt;
+        this.cantidad=cantidad;
+        this.PrecioU=PrecioU;
+    }
+
+    void mostrarArticulo(){
+        System.out.println("Articulo : "+nombreArt);
+        System.out.println("Cantidad : "+ cantidad);
+        System.out.println("PrecioU : "+PrecioU);
+    }
+
+    public String getnombreArt(){
+        return nombreArt;
+    }
     
+    public int getcantidad(){
+        return cantidad;
+    }
+    public double getPrecioU(){
+        return PrecioU;
+    }
 }
